6610d4c551fcce0a4c2613cb9b420ec9ef1fd612
Andriy Shkrobut <andriy.sshkrobut@gmail.com>
2022-06-25 17:25:42 +0300

    render shop

--- main.js
+++ main.js
@@ -3,335 +3,373 @@ const products = document.getElementById("food_products");
 const shop = document.getElementById("shop");
 const shops = document.querySelectorAll(".shop");
 const delivery = {
-  fastfoods: {
-    mcdonalds: [
-      {
-        name: "Hamburger",
-        price: 28,
-        id: 1,
-        itemNumber: 0,
-        img: "img/McHamburger.jfif",
-      },
-      {
-        name: "Cheeseburger",
-        price: 32,
-        id: 2,
-        itemNumber: 0,
-        img: "img/McCheeseburger.jfif",
-      },
-      {
-        name: "Double Cheeseburger",
-        price: 53,
-        id: 3,
-        itemNumber: 0,
-        img: "img/McDoubleCheeseburger.jfif",
-      },
-      {
-        name: "Big Mac",
-        price: 62,
-        id: 4,
-        itemNumber: 0,
-        img: "img/McBigMac.jfif",
-      },
-      {
-        name: "Big Texas",
-        price: 99,
-        id: 5,
-        itemNumber: 0,
-        img: "img/McBigTexas.jfif",
-      },
-      {
-        name: "Big Taisty",
-        price: 95,
-        id: 6,
-        itemNumber: 0,
-        img: "img/McBigTaisty.jfif",
-      },
-      {
-        name: "French fries small",
-        price: 30,
-        id: 7,
-        itemNumber: 0,
-        img: "img/McFrenchfries.jfif",
-      },
-      {
-        name: "French fries big",
-        price: 35,
-        id: 8,
-        itemNumber: 0,
-        img: "img/McFrenchfries.jfif",
-      },
-      {
-        name: "Chicken McNuggets 6 pcs.",
-        price: 55,
-        id: 9,
-        itemNumber: 0,
-        img: "img/McChickenNuggets.jfif",
-      },
-      {
-        name: "Chicken McNuggets 9 pcs.",
-        price: 72,
-        id: 10,
-        itemNumber: 0,
-        img: "img/McChickenNuggets.jfif",
-      },
-      {
-        name: "Coca-cola 0,5",
-        price: 25,
-        id: 11,
-        itemNumber: 0,
-        img: "img/coca-cola.jpg",
-      },
-      {
-        name: "Fanta 0,5",
-        price: 25,
-        id: 12,
-        itemNumber: 0,
-        img: "img/fanta.jfif",
-      },
-      {
-        name: "Juice 0,5",
-        price: 35,
-        id: 13,
-        itemNumber: 0,
-        img: "img/rich.jpg",
-      },
-    ],
-    kfs: [
-      {
-        name: "Cheeseburger",
-        price: 40,
-        id: 1,
-        itemNumber: 0,
-        img: "img/KFC-CheeseBurge.png",
-      },
-      {
-        name: "Cheeseburger De Luxe",
-        price: 62,
-        id: 2,
-        itemNumber: 0,
-        img: "img/KFC-CheeseburgerDeLuxe.jfif",
-      },
-      {
-        name: "Chefburger",
-        price: 59,
-        id: 3,
-        itemNumber: 0,
-        img: "img/KFC-Chefburger.jfif",
-      },
-      {
-        name: "Double Schefburger",
-        price: 93,
-        id: 4,
-        itemNumber: 0,
-        img: "img/KFC-DoubleSchefburger.png",
-      },
-      {
-        name: "French fries",
-        price: 36,
-        id: 5,
-        itemNumber: 0,
-        img: "img/KFS-Frenchfries.jfif",
-      },
-      {
-        name: "Junior Twister",
-        price: 45,
-        id: 6,
-        itemNumber: 0,
-        img: "img/KFC-JuniorTwister.jfif",
-      },
-      {
-        name: "Twister with toaster",
-        price: 67,
-        id: 7,
-        itemNumber: 0,
-        img: "img/KFC-Twister-with-toaster.jfif",
-      },
-      {
-        name: "Coca-cola 0,5",
-        price: 25,
-        id: 8,
-        itemNumber: 0,
-        img: "img/coca-cola.jpg",
-      },
-      {
-        name: "Fanta 0,5",
-        price: 25,
-        id: 9,
-        itemNumber: 0,
-        img: "img/fanta.jfif",
-      },
-      {
-        name: "Juice 0,5",
-        price: 35,
-        id: 10,
-        itemNumber: 0,
-        img: "img/rich.jpg",
-      },
-    ],
-    noa: [
-      {
-        name: "Midori roll",
-        price: 179,
-        id: 1,
-        itemNumber: 0,
-        img: "img/NOA-Rol-midori.png",
-      },
-      {
-        name: "Chiara's role",
-        price: 62,
-        id: 2,
-        itemNumber: 0,
-        img: "img/Noa-Rol-kiara.png",
-      },
-      {
-        name: "Roll Nobu",
-        price: 119,
-        id: 3,
-        itemNumber: 0,
-        img: "img/NOA-Rol-nobu.png",
-      },
-      {
-        name: "Roll Acaio",
-        price: 189,
-        id: 4,
-        itemNumber: 0,
-        img: "img/NOA-Rol-akajo.png",
-      },
-      {
-        name: "Roll with tuna tartare",
-        price: 209,
-        id: 5,
-        itemNumber: 0,
-        img: "img/NOA_Rol-z-tartarom-z-tunczya.png",
-      },
-      {
-        name: "Roll with salmon and shrimp",
-        price: 279,
-        id: 6,
-        itemNumber: 0,
-        img: "img/NOA-Roll-with-salmon-and-shrimp.png",
-      },
-      {
-        name: "Hot Abby with shrimp and eel",
-        price: 249,
-        id: 7,
-        itemNumber: 0,
-        img: "img/NOA-Hot-Abby-with-shrimp-and-eel.png",
-      },
-      {
-        name: "Akami roll with tuna",
-        price: 239,
-        id: 8,
-        itemNumber: 0,
-        img: "img/NOA-Akami-roll-with-tuna.png",
-      },
-      {
-        name: "Coca-cola 0,5",
-        price: 25,
-        id: 9,
-        itemNumber: 0,
-        img: "img/coca-cola.jpg",
-      },
-      {
-        name: "Fanta 0,5",
-        price: 25,
-        id: 10,
-        itemNumber: 0,
-        img: "img/fanta.jfif",
-      },
-      {
-        name: "Juice 0,5",
-        price: 35,
-        id: 11,
-        itemNumber: 0,
-        img: "img/rich.jpg",
-      },
-    ],
-    piec: [
-      {
-        name: "Formajipesto",
-        price: 219,
-        id: 1,
-        itemNumber: 0,
-        img: "img/La-Formajipesto.jpg",
-      },
-      {
-        name: "Javelin",
-        price: 215,
-        id: 2,
-        itemNumber: 0,
-        img: "img/La-Javelin.jpg",
-      },
-      {
-        name: "Pizza La Piec",
-        price: 230,
-        id: 3,
-        itemNumber: 0,
-        img: "img/La-Piec.jpg",
-      },
-      {
-        name: "Caesario",
-        price: 257,
-        id: 4,
-        itemNumber: 0,
-        img: "img/La-Tsezario.jpg",
-      },
-      {
-        name: "Quattro formage",
-        price: 241,
-        id: 5,
-        itemNumber: 0,
-        img: "img/La-Kvatro.jpg",
-      },
-      {
-        name: "Capricciosa",
-        price: 208,
-        id: 6,
-        itemNumber: 0,
-        img: "img/La-Capricciosa.jpg",
-      },
-      {
-        name: "Carbonara",
-        price: 202,
-        id: 7,
-        itemNumber: 0,
-        img: "img/La-Carbonara.jpg",
-      },
-      {
-        name: "Diabola",
-        price: 209,
-        id: 8,
-        itemNumber: 0,
-        img: "img/La-Diabola.jpg",
-      },
-      {
-        name: "Coca-cola 0,5",
-        price: 25,
-        id: 9,
-        itemNumber: 0,
-        img: "img/coca-cola.jpg",
-      },
-      {
-        name: "Fanta 0,5",
-        price: 25,
-        id: 10,
-        itemNumber: 0,
-        img: "img/fanta.jfif",
-      },
-      {
-        name: "Juice 0,5",
-        price: 35,
-        id: 11,
-        itemNumber: 0,
-        img: "img/rich.jpg",
-      },
-    ],
-  },
+  food: [
+    {
+      name: "Hamburger",
+      id: 1,
+      price: 28,
+      itemNumber: 0,
+      img: "img/McHamburger.jfif",
+      type: "mac",
+    },
+    {
+      name: "Cheeseburger",
+      id: 2,
+      price: 32,
+      itemNumber: 0,
+      img: "img/McCheeseburger.jfif",
+      type: "mac",
+    },
+    {
+      name: "Double Cheeseburger",
+      id: 3,
+      price: 53,
+      itemNumber: 0,
+      img: "img/McDoubleCheeseburger.jfif",
+      type: "mac",
+    },
+    {
+      name: "Big Mac",
+      id: 4,
+      price: 62,
+      itemNumber: 0,
+      img: "img/McBigMac.jfif",
+      type: "mac",
+    },
+    {
+      name: "Big Texas",
+      id: 5,
+      price: 99,
+      itemNumber: 0,
+      img: "img/McBigTexas.jfif",
+      type: "mac",
+    },
+    {
+      name: "Big Taisty",
+      id: 6,
+      price: 95,
+      itemNumber: 0,
+      img: "img/McBigTaisty.jfif",
+      type: "mac",
+    },
+    {
+      name: "French fries small",
+      id: 7,
+      price: 30,
+      itemNumber: 0,
+      img: "img/McFrenchfries.jfif",
+      type: "mac",
+    },
+    {
+      name: "French fries big",
+      id: 8,
+      price: 35,
+      itemNumber: 0,
+      img: "img/McFrenchfries.jfif",
+      type: "mac",
+    },
+    {
+      name: "Chicken McNuggets 6 pcs.",
+      id: 9,
+      price: 55,
+      itemNumber: 0,
+      img: "img/McChickenNuggets.jfif",
+      type: "mac",
+    },
+    {
+      name: "Chicken McNuggets 9 pcs.",
+      id: 10,
+      price: 72,
+      itemNumber: 0,
+      img: "img/McChickenNuggets.jfif",
+      type: "mac",
+    },
+    {
+      name: "Coca-cola 0,5",
+      id: 11,
+      price: 25,
+      itemNumber: 0,
+      img: "img/coca-cola.jpg",
+      type: "mac",
+    },
+    {
+      name: "Fanta 0,5",
+      id: 12,
+      price: 25,
+      itemNumber: 0,
+      img: "img/fanta.jfif",
+      type: "mac",
+    },
+    {
+      name: "Juice 0,5",
+      id: 13,
+      price: 35,
+      itemNumber: 0,
+      img: "img/rich.jpg",
+      type: "mac",
+    },
+    {
+      name: "Cheeseburger",
+      id: 14,
+      price: 40,
+      itemNumber: 0,
+      img: "img/KFC-CheeseBurge.png",
+      type: "kfc",
+    },
+    {
+      name: "Cheeseburger De Luxe",
+      id: 15,
+      price: 62,
+      itemNumber: 0,
+      img: "img/KFC-CheeseburgerDeLuxe.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Chefburger",
+      id: 16,
+      price: 59,
+      itemNumber: 0,
+      img: "img/KFC-Chefburger.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Double Schefburger",
+      id: 17,
+      price: 93,
+      itemNumber: 0,
+      img: "img/KFC-DoubleSchefburger.png",
+      type: "kfc",
+    },
+    {
+      name: "French fries",
+      id: 18,
+      price: 36,
+      itemNumber: 0,
+      img: "img/KFS-Frenchfries.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Junior Twister",
+      id: 19,
+      price: 45,
+      itemNumber: 0,
+      img: "img/KFC-JuniorTwister.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Twister with toaster",
+      id: 20,
+      price: 67,
+      itemNumber: 0,
+      img: "img/KFC-Twister-with-toaster.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Coca-cola 0,5",
+      id: 21,
+      price: 25,
+      itemNumber: 0,
+      img: "img/coca-cola.jpg",
+      type: "kfc",
+    },
+    {
+      name: "Fanta 0,5",
+      id: 22,
+      price: 25,
+      itemNumber: 0,
+      img: "img/fanta.jfif",
+      type: "kfc",
+    },
+    {
+      name: "Juice 0,5",
+      id: 23,
+      price: 35,
+      itemNumber: 0,
+      img: "img/rich.jpg",
+      type: "kfc",
+    },
+    {
+      name: "Midori roll",
+      id: 24,
+      price: 179,
+      itemNumber: 0,
+      img: "img/NOA-Rol-midori.png",
+      type: "noa",
+    },
+    {
+      name: "Chiara's role",
+      id: 25,
+      price: 62,
+      itemNumber: 0,
+      img: "img/Noa-Rol-kiara.png",
+      type: "noa",
+    },
+    {
+      name: "Roll Nobu",
+      id: 26,
+      price: 119,
+      itemNumber: 0,
+      img: "img/NOA-Rol-nobu.png",
+      type: "noa",
+    },
+    {
+      name: "Roll Acaio",
+      id: 27,
+      price: 189,
+      itemNumber: 0,
+      img: "img/NOA-Rol-akajo.png",
+      type: "noa",
+    },
+    {
+      name: "Roll with tuna tartare",
+      id: 28,
+      price: 209,
+      itemNumber: 0,
+      img: "img/NOA_Rol-z-tartarom-z-tunczya.png",
+      type: "noa",
+    },
+    {
+      name: "Roll with salmon and shrimp",
+      id: 29,
+      price: 279,
+      itemNumber: 0,
+      img: "img/NOA-Roll-with-salmon-and-shrimp.png",
+      type: "noa",
+    },
+    {
+      name: "Hot Abby with shrimp and eel",
+      id: 30,
+      price: 249,
+      itemNumber: 0,
+      img: "img/NOA-Hot-Abby-with-shrimp-and-eel.png",
+      type: "noa",
+    },
+    {
+      name: "Akami roll with tuna",
+      id: 31,
+      price: 239,
+      itemNumber: 0,
+      img: "img/NOA-Akami-roll-with-tuna.png",
+      type: "noa",
+    },
+    {
+      name: "Coca-cola 0,5",
+      id: 32,
+      price: 25,
+      itemNumber: 0,
+      img: "img/coca-cola.jpg",
+      type: "noa",
+    },
+    {
+      name: "Fanta 0,5",
+      id: 33,
+      price: 25,
+      itemNumber: 0,
+      img: "img/fanta.jfif",
+      type: "noa",
+    },
+    {
+      name: "Juice 0,5",
+      id: 34,
+      price: 35,
+      itemNumber: 0,
+      img: "img/rich.jpg",
+      type: "noa",
+    },
+    {
+      name: "Formajipesto",
+      id: 35,
+      price: 219,
+      itemNumber: 0,
+      img: "img/La-Formajipesto.jpg",
+      type: "piec",
+    },
+    {
+      name: "Javelin",
+      id: 36,
+      price: 215,
+      itemNumber: 0,
+      img: "img/La-Javelin.jpg",
+      type: "piec",
+    },
+    {
+      name: "Pizza La Piec",
+      id: 37,
+      price: 230,
+      itemNumber: 0,
+      img: "img/La-Piec.jpg",
+      type: "piec",
+    },
+    {
+      name: "Caesario",
+      id: 38,
+      price: 257,
+      itemNumber: 0,
+      img: "img/La-Tsezario.jpg",
+      type: "piec",
+    },
+    {
+      name: "Quattro formage",
+      id: 39,
+      price: 241,
+      itemNumber: 0,
+      img: "img/La-Kvatro.jpg",
+      type: "piec",
+    },
+    {
+      name: "Capricciosa",
+      id: 40,
+      price: 208,
+      itemNumber: 0,
+      img: "img/La-Capricciosa.jpg",
+      type: "piec",
+    },
+    {
+      name: "Carbonara",
+      id: 41,
+      price: 202,
+      itemNumber: 0,
+      img: "img/La-Carbonara.jpg",
+      type: "piec",
+    },
+    {
+      name: "Diabola",
+      id: 42,
+      price: 209,
+      itemNumber: 0,
+      img: "img/La-Diabola.jpg",
+      type: "piec",
+    },
+    {
+      name: "Coca-cola 0,5",
+      id: 43,
+      price: 25,
+      itemNumber: 0,
+      img: "img/coca-cola.jpg",
+      type: "piec",
+    },
+    {
+      name: "Fanta 0,5",
+      id: 44,
+      price: 25,
+      itemNumber: 0,
+      img: "img/fanta.jfif",
+      type: "piec",
+    },
+    {
+      name: "Juice 0,5",
+      id: 45,
+      price: 35,
+      itemNumber: 0,
+      img: "img/rich.jpg",
+      type: "piec",
+    },
+  ],
   renderProducts(shop) {
     if (this.cart.items.length === 0) {
+      const store = this.food.filter((item) => item.type === shop);
       products.innerHTML = "";
-      this.fastfoods[shop].map((product) => {
+      store.map((product) => {
         const productCard = `
       <div class="item" data-product-id="${product.id}">
       <article class="productCard">
@@ -339,9 +377,9 @@ const delivery = {
         <h2 class="productName">${product.name}</h2>
           <div class="price_buy">
             <span class="price_product">${product.price.toFixed(2)}$</span>
-            <button class="buy" id="buy" data-${shop}="${
-          product.id
-        }">Add to cart</button>
+            <button class="buy" id="buy" data-id="${
+              product.id
+            }">Add to cart</button>
           </div>
       </article>
       </div>
@@ -352,47 +390,60 @@ const delivery = {
   },
   cart: {
     items: [],
-    add(shop, id) {
-      const { items } = this;
-      console.log(items);
-      items.map((item) => {
-        console.log(item.id, id);
-        console.log(item);
-        if (item.id === id) {
-          item.quantity += 1;
+    setLocal() {
+      localStorage.setItem("cart", JSON.stringify(this.items));
+    },
+    getLocal() {
+      return JSON.parse(localStorage.getItem("cart"));
+    },
+    add(id) {
+      for (const itemInCart of this.items) {
+        if (itemInCart.id === id) {
+          itemInCart.quantity += 1;
           return;
         }
-      });
-      delivery.fastfoods[shop].map((item) => {
-        if (item.id == id) {
+      }
+      delivery.food.forEach((item) => {
+        if (id === item.id) {
           const newProduct = {
             ...item,
             quantity: 1,
           };
-          items.push(newProduct);
+          this.items.push(newProduct);
         }
       });
+    },
+    clean() {
+      this.items = [];
     },
   },
 };
 shop.addEventListener("click", (e) => {
-  if (delivery.cart.items.length === 0) {
+  function changeStore() {
     const name = e.target.dataset.shopName;
     for (const item of shops) {
       item.classList.remove("active");
     }
     e.target.classList.add("active");
-    delivery.renderProducts(name);
+    name && delivery.renderProducts(name);
+  }
+  if (delivery.cart.items.length === 0) {
+    const name = e.target.dataset.shopName;
+    changeStore();
   } else {
-    alert("You can only deliver from one store");
-    const us = confirm("clean cart");
-    console.log(us);
-    
+    const us = confirm(
+      "Sorry, delivery from one store is possible, do you want to clear the cart ??"
+    );
+    if (us) {
+      delivery.cart.clean();
+      changeStore();
+      delivery.cart.setLocal();
+    }
   }
 });
 products.addEventListener("click", (e) => {
-  const shop = String(Object.keys(e.target.dataset));
-  const id = +e.target.dataset[shop];
-  delivery.cart.add(shop, id);
-  console.log(shop);
+  const id = +e.target.dataset.id;
+  delivery.cart.add(id);
+  console.log(delivery.cart.items);
+  delivery.cart.setLocal();
 });
